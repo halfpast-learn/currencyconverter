c0201f5df5805a78f430052f13eac5730adb3aeb
Andrey_Aleshin <andrej.aleshin1121@gmail.com>
2022-02-10 18:38:37 +0300

    transfer ui from aura

--- force-app/main/default/lwc/currencyConverterLWC/currencyConverterLWC.js
+++ force-app/main/default/lwc/currencyConverterLWC/currencyConverterLWC.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class CurrencyConverterLWC extends LightningElement {}
+export default class CurrencyConverterLWC extends LightningElement {
+    firstAmount;
+    secondAmount;
+    selectedCurrency2;
+    selectedCurrency1
+    convert(event) { console.log("convert worked, event:\n" + event); }
+}
