7b852d20a9a62236d16fb24db2ab03971a68624d
aalesh <aalesh@ct.c>
2022-02-11 18:18:12 +0300

    populate select

--- force-app/main/default/lwc/currencyConverterLWC/currencyConverterLWC.js
+++ force-app/main/default/lwc/currencyConverterLWC/currencyConverterLWC.js
@@ -4,6 +4,16 @@ export default class CurrencyConverterLWC extends LightningElement {
     firstAmount;
     secondAmount;
     selectedCurrency2;
-    selectedCurrency1
+    selectedCurrency1;
+    rates;
+    currencies;
     convert(event) { }
+    doInit(event) {
+        const api = "https://api.exchangerate-api.com/v4/latest/USD";
+        fetch(api).then(response => response.json())
+            .then(data => {
+                currencies=Object.keys(data.rates);
+                rates=data.rates;
+            });
+    }
 }
