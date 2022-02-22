9ec5f05b980ef030ff1ade8f98c15495e246953d
Andrey_Aleshin <andrej.aleshin1121@gmail.com>
2022-02-22 19:52:02 +0300

    remove reduntant

--- force-app/main/default/aura/currencyConverter/currencyConverterController.js
+++ force-app/main/default/aura/currencyConverter/currencyConverterController.js
@@ -1,29 +1,28 @@
 ({
     doInit: function (component, event, helper) {
-        const api = "https://api.exchangerate-api.com/v4/latest/USD";
-        fetch(api).then(response => response.json())
+        fetch('https://api.exchangerate-api.com/v4/latest/USD').then(response => response.json())
             .then(data => {
-                component.set("v.currencyList", Object.keys(data.rates));
-                component.set("v.rates", data.rates);
+                component.set('v.currencyList', Object.keys(data.rates));
+                component.set('v.rates', data.rates);
             });
     },
     convert: function (component, event, helper) {
-        var firstCurrency = component.get("v.selectedCurrency1");
-        var secondCurrency = component.get("v.selectedCurrency2");
+        var firstCurrency = component.get('v.selectedCurrency1');
+        var secondCurrency = component.get('v.selectedCurrency2');
 
-        var rates = component.get("v.rates");
+        var rates = component.get('v.rates');
         var result = 0;
-        if (event.getSource().get("v.name") == "secondValue") {
-            var secondAmount = component.get("v.secondAmount");
+        if (event.getSource().get('v.name') == 'secondValue') {
+            var secondAmount = component.get('v.secondAmount');
             result = helper.calculateConversion(secondAmount, rates[secondCurrency], rates[firstCurrency]);
             if (!isNaN(result))
-                component.set("v.firstAmount", result.toFixed(2));
+                component.set('v.firstAmount', result.toFixed(2));
         }
         else {
-            var firstAmount = component.get("v.firstAmount");
+            var firstAmount = component.get('v.firstAmount');
             result = helper.calculateConversion(firstAmount, rates[firstCurrency], rates[secondCurrency]);
             if (!isNaN(result))
-                component.set("v.secondAmount", result.toFixed(2));
+                component.set('v.secondAmount', result.toFixed(2));
         }
     }
 })
