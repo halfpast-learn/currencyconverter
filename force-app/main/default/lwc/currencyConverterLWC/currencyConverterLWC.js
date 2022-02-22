19e0d85aeda2c0e12b062636679e67f65b34fa5f
Andrey_Aleshin <andrej.aleshin1121@gmail.com>
2022-02-22 14:15:24 +0300

    convert works

--- force-app/main/default/lwc/currencyConverterLWC/currencyConverterLWC.js
+++ force-app/main/default/lwc/currencyConverterLWC/currencyConverterLWC.js
@@ -1,40 +1,74 @@
 import { api, LightningElement } from 'lwc';
 
 export default class CurrencyConverterLWC extends LightningElement {
-    firstAmount;
-    secondAmount;
-    selectedCurrency2;
-    selectedCurrency1;
+    @api firstAmount;
+    @api secondAmount;
+    @api firstSelectedCurrency;
+    @api secondSelectedCurrency;
     rates;
-    @api currencies = [{id:"abc1", name:"usd1"}];
-    convert(event) { }
+    @api currencies = [];
+
     connectedCallback() {
         const api = "https://api.exchangerate-api.com/v4/latest/USD";
-        console.log(this.currencies[0].id,this.currencies[0].name);
-        /*fetch(api).then(response => response.json())
+        fetch(api)
+            .then(response => response.json())
             .then(data => {
-                this.currencies = Object.keys(data.rates);
+                this.currencies = Object.keys(data.rates).map(rate => ({ label: rate, value: rate }));
                 this.rates = data.rates;
-                console.log(this.rates);
-            });*/
-    }
-    //TODO:
-    //does pre-populated list work?
-    //try wiring: https://www.sfdcpoint.com/salesforce/foreach-template-directives-in-lwc/
-    /*get currencyNames() {
-        const api = "https://api.exchangerate-api.com/v4/latest/USD";
-        fetch(api).then(response => response.json())
-            .then(data => {
-                this.currencies = Object.keys(data.rates);
-                this.rates = data.rates;
-                console.log(this.rates);
-
-                let result = [];
-                for (let i = 0; i < this.currencies.length; i++) {
-                    result.push({ name: this.currencies[i] });
-                }
-                console.log(result);
-                return result;
             });
-    }*/
+    }
+
+    calculateConversion(from, rateFirst, rateSecond) {
+        var valueInUSD = parseFloat(from) / parseFloat(rateFirst);
+        var result = valueInUSD * parseFloat(rateSecond);
+        return result;
+    }
+
+    convert(event) {
+        var result = 0;
+        if (event.target.name == "secondAmount") {
+            result = this.calculateConversion(this.secondAmount, this.rates[this.secondSelectedCurrency], this.rates[this.firstSelectedCurrency]);
+            if (!isNaN(result))
+                this.firstAmount = result.toFixed(2);
+        }
+        else {
+            result = this.calculateConversion(this.firstAmount, this.rates[this.firstSelectedCurrency], this.rates[this.secondSelectedCurrency]);
+            if (!isNaN(result))
+                this.secondAmount = result.toFixed(2);
+        }
+    }
+
+    checkIfShouldConvert() {
+        if (isNaN(this.rates[this.firstSelectedCurrency])
+            || isNaN(this.rates[this.secondSelectedCurrency])) {
+            return false;
+        }
+        else if (isNaN(this.firstAmount) && isNaN(this.secondAmount)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    handleChange(event) {
+        var caller = event.target.name;
+        var val = event.target.value;
+
+        if (caller == 'firstAmount')
+            this.firstAmount = val;
+        else if (caller == 'secondAmount')
+            this.secondAmount = val;
+        else if (caller == 'firstSelectedCurrency')
+            this.firstSelectedCurrency = val;
+        else if (caller == 'secondSelectedCurrency')
+            this.secondSelectedCurrency = val;
+
+        if (this.checkIfShouldConvert()) {
+            this.convert(event);
+        }
+    }
+
+
+
 }
